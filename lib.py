1764a23db60a06a64627c31d5054d75681652f54
Asheesh Laroia <asheesh@asheesh.org>
2016-01-15 22:32:39 -0500

    wip

--- lib.py
+++ lib.py
@@ -3,11 +3,41 @@
 # You can also execute this file if you want; it'll run the unit tests for library functions.
 
 def decode_ascii(bytes):
-    # TODO: Make less of a skeleton.
-    return bytes.decode('ascii')
+    # Try AND-ing against top bit set to on. If the result has the top
+    # bit set after an AND, then the input had the top bit set to
+    # 1. So reject if so.
+    if len(bytes) > 1:
+        raise ValueError("this one does one byte at a time")
+    mask_ = 0b10000000
+    first_char = ord(bytes[0])
+    masked = mask_ & first_char
+    if masked != mask_:
+        return unicode(chr(first_char), 'ascii')
+    raise ValueError()
+
+def decode_two_byte_utf8(bytes):
+    mask_ = 0b11100000
+    valid = 0b11000000
+    first_char = ord(bytes[0])
+    masked = mask_ & first_char
+    if masked == valid:
+        print 'This should be 2-char utf-8'
+        import pdb; pdb.set_trace()
+        return unicode(chr(first_char), 'ascii')
+    raise ValueError("Too bad, did not decode")
 
 def decode_normal_utf8(bytes):
     return bytes.decode('utf-8')
+
+def decode_overlong_two_bytes_utf8(bytes):
+    pass
+
+def decode_overlong_three_bytes_utf8(bytes):
+    pass
+
+def decode_overlong_four_bytes_utf8(bytes):
+    pass
+
 
 
 TRY_TO_DECODE_WITH_THESE = [
@@ -35,6 +65,11 @@ def actual_main():
 
 ###
 import unittest
+
+class Tests(unittest.TestCase):
+    def test_ascii(self):
+        self.assertEqual(u'a', decode_ascii('a'))
+
 
 if __name__ == '__main__':
     unittest.main()
